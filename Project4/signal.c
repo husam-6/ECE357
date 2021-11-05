9878ebf5415a4db4bdca624bccf472fd4f266b34
Husam Almanakly <husam@Husams-Macbook.local>
2021-11-05 18:28:22 -0400

    Fix RT bug

--- Project4/signal.c
+++ Project4/signal.c
@@ -1,52 +1,108 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
+#include <string.h>
+#include <sys/wait.h>
 
+int handlerCount;
+int handlerCountRT;
 
-
-int sendSig(ppid, sends)
+//Function for child processes to send each signal 
+//the specified number of times 
+int sendSig(int ppid, int sends)
 {
     for(int i = 0; i<sends; i++)
     {
         kill(ppid, SIGINT);
-        kill(ppid, SIGINT);
+        kill(ppid, SIGRTMIN+1);
+        // printf("testing\n");
     }
+    return 0; 
 }
 
-
-int main(int argc, int *argv[])
+//Simplistic handler to demonstrate the number of 
+//times the signal is handled
+void handler(int sig)
 {
-    pid_t childPids[10];
-    int n = argv[1];
-    int sends = argv[2];
+    ++handlerCount;
+}
+
+//Handler foro the real time signal
+void handlerRT(int sig)
+{
+    ++handlerCountRT;
+}
+
+int main(int argc, char *argv[])
+{
+    //Set up signal handlers for SIGINT and real time signal 35
+    struct sigaction saINT;
+    saINT.sa_handler=handler;
+    saINT.sa_flags=SA_NODEFER;
+    sigemptyset(&saINT.sa_mask);
     
+    struct sigaction saRT; 
+    saRT.sa_handler = handlerRT; 
+    saRT.sa_flags = SA_NODEFER; 
+    sigemptyset(&saRT.sa_mask);
+
+    (void)sigaction(SIGINT,&saINT,NULL);
+    (void)sigaction(SIGRTMIN+1, &saRT, NULL);
+
+    // set up array to store all the child pid's  
+    pid_t childPids[100000];
+    int n, sends; 
+    if(argv[1] && argv[2])
+    {
+        n = atoi(argv[1]);
+        sends = atoi(argv[2]);
+    }
+    // default to 1 child and 1 send
+    else
+    {
+        n = 1; 
+        sends = 1;
+    }
+
+    //printf("%d          %d\n", n, sends);
+
     // Start n children specified by user
     for (int i = 0; i < n; i++) 
-    {
+    {   
+        int ppid; 
         switch (childPids[i] = fork())
         {
             case -1:
                 fprintf(stderr, "Error processing command: %s", strerror(errno));
                 break;
             case 0:
-                int ppid = getppid();
+                // printf("CHILD PROCESS\n");
+                ppid = getppid();
                 sendSig(ppid, sends);
-                break; 
+                return 0; 
             case 1: 
                 break; 
         } 
     }
 
-    //Wait for parents to exit
+    //Wait for each child to exit
     int status;
     pid_t pid;
-    while (n > 0) 
+
+    int tmp = n; 
+    while (tmp > 0) 
     {
         pid = wait(&status);
-        printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
-        --n;  // TODO(pts): Remove pid from the pids array.
+        // printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
+        --tmp;  // TODO(pts): Remove pid from the pids array.
     }
+
+    // Print results: number of times the signal was sent and 
+    // how many times it was handled
+    printf("Signals delivered: %d, signals handled: %d\n", n*sends, handlerCount);
+    printf("Real time signals delivered: %d, signals handled: %d\n", n*sends, handlerCountRT);
 
     return 0; 
 }
